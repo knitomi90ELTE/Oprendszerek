0eed0a3abb090ada1f35d14a0890e3818ad931e0
Knizner Tamás <knitomi90@inf.elte.hu>
2016-04-04 23:54:26 +0200

    done

--- main.c
+++ main.c
@@ -22,6 +22,8 @@ int get_command(const char* command){
         return 2;
     } else if(strcmp(command, "signup") == 0) {
         return 3;
+    } else if(strcmp(command, "details") == 0){
+        return 4;
     } else {
         return -1;
     }
@@ -74,7 +76,6 @@ void add_destination(char* dest){
         exit(1);
     }
     char c;
-    int i;
     while(true){
         if(read(f, &c, sizeof(c)) == 0){
             strncat(dest,NEWLINE_C,1);
@@ -101,51 +102,55 @@ void list_destinations(){
 }
 
 bool validateDest(char* dest){
-    printf("validating\n");
+    printf("validating...\n");
     bool l = false;
     int f = open("destination",O_RDONLY);
     if (f < 0){
         perror("Error at opening the file\n");
         exit(1);
     }
-
-
-    char* d2 = malloc(32*sizeof(char));
-    int n = sprintf(d2, "%s\n",dest);
-    d2 = realloc(d2,n);
-    printf("d2: %s",d2);
-
-    char* buffer = malloc(32*sizeof(char));;
+    char* d2 = malloc(1);
+    sprintf(d2, "%s\n",dest);
+    char* buffer = malloc(1);
     char c;
-
     while (read(f, &c, sizeof(c))){
-
-        //buffer = (char*)malloc(sizeof(buffer)+sizeof(c));
-        strncat(buffer,&c,1);
+        sprintf(buffer, "%s%c",buffer,c);
         if(c == NEWLINE_I){
-            printf("%s",buffer);
             if(strcmp(buffer, d2) == 0){
                 l = true;
             }
-            //free(buffer);
-            //buffer = realloc(buffer,sizeof(char));
-
+            strcpy(buffer,"");
         }
     }
-    printf("\n");
+    free(buffer);
+    free(d2);
     close(f);
     return l;
 }
 
+void appendToFile(char* file,char* text){
+    printf("appendToFile\n");
+    int f = open(file,O_RDWR|O_CREAT,S_IRUSR|S_IWUSR);
+    if (f < 0){
+        perror("Error at opening the file\n");
+        exit(1);
+    }
+    char c;
+    while(true){
+        if(read(f, &c, sizeof(c)) == 0){
+            write(f, text,strlen(text));
+            break;
+        }
+    }
+    close(f);
+}
+
 void signup(char ** data){
     printf("signup\n");
-    char* fullTime = malloc(32*sizeof(char));
+    char* fullTime = malloc(sizeof(char));
     time_t t = time(NULL);
     struct tm tm = *localtime(&t);
-    int n = sprintf(fullTime, "%d-%d-%d %d:%d:%d", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
-    fullTime = realloc(fullTime,n);
-    printf("%s\n",fullTime);
-
+    sprintf(fullTime, "%d-%d-%d %d:%d:%d", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
     /*
      data[0]: ./a.out
      data[1]: kapcsoló
@@ -154,55 +159,51 @@ void signup(char ** data){
      data[4]: Célállomás
      data[5]: Telefon
      */
-
     char* lastName = data[2];
     char* firstName = data[3];
     char* dest = data[4];
     char* phone = data[5];
-
     if(validateDest(dest)){
-
+        printf("CELALLOMAS OK\n");
         char* p = "passengers_";
-        int size = strlen(p) + strlen(dest);
-
+        int size = (int)strlen(p) + (int)strlen(dest);
         char* def = (char*)malloc(size);
-
         sprintf(def,"%s%s",p,dest);
-
-        printf("%s\n",def);
-        int newFile = open(def,O_RDWR|O_CREAT|O_TRUNC,S_IRUSR|S_IWUSR);
-        if (newFile < 0){
-            perror("Error at opening the file\n");
-            exit(1);
-        }
-        char fullString[sizeof(lastName)+sizeof(firstName)+sizeof(dest)+sizeof(phone)+sizeof(fullTime)];
+        char fullString[sizeof(lastName)+sizeof(firstName)+sizeof(dest)+sizeof(phone)+sizeof(fullTime)+1];
         sprintf(fullString,"%s %s %s %s %s\n",lastName, firstName, dest, phone, fullTime);
-
         printf("%s",fullString);
-
-        write(newFile, fullString,sizeof(fullString));
-        /*
-        free(fullString);
+        appendToFile(def, fullString);
         free(def);
-        free(p);
-         */
-        close(newFile);
     }else{
         printf("Nincs ilyen celallomas: %s\n",dest);
     }
-    /*
-    free(lastName);
-    free(firstName);
-    free(dest);
-    free(phone);
     free(fullTime);
-    */
+}
+
+void details(char *dest) {
+    char* p = "passengers_";
+    int size = (int)strlen(p) + (int)strlen(dest);
+    char* def = (char*)malloc(size);
+    sprintf(def,"%s%s",p,dest);
+
+    int f = open(def, O_RDONLY);
+    if (f < 0){
+        //perror("Error at opening the file\n");
+        printf("Nincs adat ehhez a jarathoz\n");
+        exit(1);
+    }
+    char c;
+    while (read(f, &c, sizeof(c))){
+        printf("%c",c);
+    }
+    close(f);
+
 
 }
 
 int main(int argc,char ** argv)
 {
-    printf("Number of command line arguments are: %i\n",argc);
+    //printf("Number of command line arguments are: %i\n",argc);
     switch (get_command(argv[1])){
         case 0:
             if(argc != 3){
@@ -224,7 +225,6 @@ int main(int argc,char ** argv)
             } else {
                 list_destinations();
             }
-
             break;
         case 3:
             if(argc != 6){
@@ -232,12 +232,18 @@ int main(int argc,char ** argv)
             } else {
                 signup(argv);
             }
-
+            break;
+        case 4:
+            if(argc != 3){
+                printf("Too few arguments!\n");
+            } else {
+                details(argv[2]);
+            }
             break;
         case -1:
             printf("Invalid argument!\n");
             break;
     }
     return 0;
-
 }
+
